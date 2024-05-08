5a48d56eaeec6dc08d17aee7f0b4ba5754b6eef9
Zifeng Niu <75737554+ZifengNiu@users.noreply.github.com>
2024-05-08 22:41:24 +0100

    upload inits.py

---
+++ inits.py
@@ -0,0 +1,25 @@
+import math
+from typing import Any
+from torch import Tensor
+
+def glorot(value: Any):
+    if isinstance(value, Tensor):
+        stdv = math.sqrt(6.0 / (value.size(-2) + value.size(-1)))
+        value.data.uniform_(-stdv, stdv)
+    else:
+        for v in value.parameters() if hasattr(value, 'parameters') else []:
+            glorot(v)
+        for v in value.buffers() if hasattr(value, 'buffers') else []:
+            glorot(v)
+
+def constant(value: Any, fill_value: float):
+    if isinstance(value, Tensor):
+        value.data.fill_(fill_value)
+    else:
+        for v in value.parameters() if hasattr(value, 'parameters') else []:
+            constant(v, fill_value)
+        for v in value.buffers() if hasattr(value, 'buffers') else []:
+            constant(v, fill_value)
+
+def zeros(value: Any):
+    constant(value, 0.)
